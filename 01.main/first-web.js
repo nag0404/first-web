4ffaf2d534abc3c6944a50d656737b5c4feb0c8d
nag0404 <981026_ng@naver.com>
2024-04-09 17:01:55 +0900

    레이아웃 전체 수정

---
+++ 01.main/first-web.js
@@ -0,0 +1,14 @@
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollTopBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollTopBtn").style.display = "none";
+    }
+}
+
+document.getElementById("scrollTopBtn").addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+});
+
