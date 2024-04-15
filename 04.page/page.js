ffa5eee99eeba90263c34949bb6dc2711af8831b
nag0404 <981026_ng@naver.com>
2024-04-15 16:58:40 +0900

    상세페이지 레이아웃 생성 및 정리

---
+++ 04.page/page.js
@@ -0,0 +1,35 @@
+/*----하단 스크롤버튼 자바스크립트----*/
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollTopBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollTopBtn").style.display = "none";
+    }
+}
+
+document.getElementById("scrollTopBtn").addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+});
+
+
+function scrollToInfo() {
+    let contentElement = document.querySelector('.info');
+    contentElement.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToGuide() {
+    let contentElement = document.querySelector('#guide');
+    contentElement.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToReview() {
+    let contentElement = document.querySelector('.info');
+    contentElement.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToQna() {
+    let contentElement = document.querySelector('.review');
+    contentElement.scrollIntoView({ behavior: 'smooth' });
+}
